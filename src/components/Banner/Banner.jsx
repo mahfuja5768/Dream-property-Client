8e2dae322cf80a02628c84359501a540d1eeee98
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2024-01-06 17:05:33 +0600

    change banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,24 @@
 import { FaArrowCircleRight } from "react-icons/fa";
-import img1 from "../../../src/assets/images/b5.webp";
-import img2 from "../../../src/assets/images/b2.webp";
+import img1 from "../../../src/assets/images/newBanner2.webp";
+import img2 from "../../../src/assets/images/newBanner.webp";
 const Banner = () => {
   return (
     <div>
-      <div className="carousel w-full h-[90vh] mb-12 text-center lg:text-start ">
+      <div className="carousel w-full h-[90vh] mb-12 text-center  ">
         <div id="slide1" className="carousel-item relative w-full">
           <img src={img1} className="w-full" alt="banner-png" />
-          <div className="absolute flex justify-between items-center transform -translate-y-1/2 left-0 right-0 top-1/2 bg-gradient-to-b from-[#121212] h-full">
+          <div className="absolute flex justify-between items-center transform -translate-y-1/2 left-0 right-0 top-1/2 bg-gradient-to-b from-[#000000] h-full">
             <a
               href="#slide2"
               className="btn btn-circle bg-[#272626] border-none text-white ms-5"
             >
               ❮
             </a>
-            <div className="hero-content text-white">
+            <div className="hero-content container text-white mx-auto flex justify-center items-center flex-col">
               <div className="grid grid-cols-1 lg:grid-cols-4">
                 <div
-                  className=" lg:col-span-3"
-                  data-aos="fade-right"
+                  className=" lg:col-span-3 "
+                  data-aos="fade-up"
                   data-aos-duration="3000"
                 >
                   <h1 className="mb-4 text-3xl md:text-5xl leading-9 font-bold w-full  md:w-10/12  lg:w-full mx-auto">
